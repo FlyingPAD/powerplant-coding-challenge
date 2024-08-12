c36f19729c02431ae6d83b18d2e611c349954584
FlyingPAD <99176621+FlyingPAD@users.noreply.github.com>
2024-08-12 09:35:39 +0200

    Initial commit

---
+++ powerplant-coding-challenge/Controllers/ProductionPlanController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PowerplantCodingChallenge.Controllers;
+
+[ApiController]
+[Route("productionplan")]
+public class ProductionPlanController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return Ok("coucou");
+    }
+}
