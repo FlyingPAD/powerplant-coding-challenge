4cf8ddf51892a2970e0960f6e6f42f1dec7ef7a3
FlyingPAD <99176621+FlyingPAD@users.noreply.github.com>
2024-08-26 16:42:37 +0200

    Refactoring

--- powerplant-coding-challenge/Features/ProductionPlanCommandHandler.cs
+++ powerplant-coding-challenge/Features/ProductionPlanCommandHandler.cs
@@ -1,36 +1,14 @@
-﻿using FluentValidation;
-using MediatR;
-using powerplant_coding_challenge.Helpers;
+﻿using MediatR;
 using powerplant_coding_challenge.Services;
-using Serilog;
 
 namespace powerplant_coding_challenge.Features;
 
-public class ProductionPlanCommandHandler(ProductionPlanService productionManager, IValidator<ProductionPlanCommand> validator) : IRequestHandler<ProductionPlanCommand, List<ProductionPlanCommandResponse>>
+public class ProductionPlanCommandHandler(ProductionPlanService productionManager) : IRequestHandler<ProductionPlanCommand, List<ProductionPlanCommandResponse>>
 {
     private readonly ProductionPlanService _productionManager = productionManager;
-    private readonly IValidator<ProductionPlanCommand> _validator = validator;
 
     public async Task<List<ProductionPlanCommandResponse>> Handle(ProductionPlanCommand command, CancellationToken cancellationToken)
     {
-        // Validation
-        var validationResult = await _validator.ValidateAsync(command, cancellationToken);
-        if (!validationResult.IsValid)
-        {
-            Log.Warning("Validation failed for production plan command: {Errors}", validationResult.Errors);
-            throw new ValidationException(validationResult.Errors);
-        }
-
-        Log.Information("Processing production plan for load: {Load} MWh with {PowerplantsCount} powerplants.", command.Load, command.Powerplants.Count);
-
-        // Generate Production Plan
-        var response = _productionManager.GenerateProductionPlan(command);
-
-        // Calculate and log total production and cost
-        var totalProduction = response.Sum(r => r.Power);
-        var totalCost = response.Sum(r => r.Power * command.Fuels.Gas);
-        LoggingHelper.LogFinalSummary(totalProduction, totalCost);
-
-        return response;
+        return _productionManager.GenerateProductionPlan(command);
     }
 }
